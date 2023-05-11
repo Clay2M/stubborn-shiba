1cf9db9991da9757a7997852c87abd4d7bbc7913
Eric <43562443+ericance@users.noreply.github.com>
2023-05-10 23:34:28 -0500

    Added a mask to the collisions

--- runner.py
+++ runner.py
@@ -1,6 +1,7 @@
 import pygame
 from sys import exit
 from random import randint, choice
+import pygame.mask
 
 jump_debounce = True
 flip_debounce = True
@@ -8,139 +9,153 @@ start_time = 0
 current_score = 0
 
 class Shiba(pygame.sprite.Sprite):
-    def __init__(self) -> None:
-        super().__init__()
-        # Importing frames for running animation
-        shiba_running1 = pygame.image.load('assets/graphics/shiba/running/running_1.png').convert_alpha()
-        shiba_running1 = pygame.transform.scale2x(shiba_running1)
-        shiba_running2 = pygame.image.load('assets/graphics/shiba/running/running_2.png').convert_alpha()
-        shiba_running2 = pygame.transform.scale2x(shiba_running2)
-        shiba_running3 = pygame.image.load('assets/graphics/shiba/running/running_3.png').convert_alpha()
-        shiba_running3 = pygame.transform.scale2x(shiba_running3)
-        shiba_running4 = pygame.image.load('assets/graphics/shiba/running/running_4.png').convert_alpha()
-        shiba_running4 = pygame.transform.scale2x(shiba_running4)
-        shiba_running5 = pygame.image.load('assets/graphics/shiba/running/running_5.png').convert_alpha()
-        shiba_running5 = pygame.transform.scale2x(shiba_running5)
-        self.shiba_running = [shiba_running1, shiba_running2, shiba_running3, shiba_running4, shiba_running5]
-        self.shiba_running_index = 0
+	def __init__(self) -> None:
+		super().__init__()
 
-        # Importing frames for jumping animation
-        shiba_jumping1 = pygame.image.load('assets/graphics/shiba/jumping/jumping_1.png').convert_alpha()
-        shiba_jumping1 = pygame.transform.scale2x(shiba_jumping1)
-        shiba_jumping2 = pygame.image.load('assets/graphics/shiba/jumping/jumping_2.png').convert_alpha()
-        shiba_jumping2 = pygame.transform.scale2x(shiba_jumping2)
-        shiba_jumping3 = pygame.image.load('assets/graphics/shiba/jumping/jumping_3.png').convert_alpha()
-        shiba_jumping3 = pygame.transform.scale2x(shiba_jumping3)
-        shiba_jumping4 = pygame.image.load('assets/graphics/shiba/jumping/jumping_4.png').convert_alpha()
-        shiba_jumping4 = pygame.transform.scale2x(shiba_jumping4)
-        shiba_jumping5 = pygame.image.load('assets/graphics/shiba/jumping/jumping_5.png').convert_alpha()
-        shiba_jumping5 = pygame.transform.scale2x(shiba_jumping5)
-        self.shiba_jumping = [shiba_jumping1, shiba_jumping2, shiba_jumping3, shiba_jumping4, shiba_jumping5]
-        self.shiba_jumping_index = 0
+		# Importing frames for running animation
+		shiba_running1 = pygame.image.load('assets/graphics/shiba/running/running_1.png').convert_alpha()
+		shiba_running1 = pygame.transform.scale2x(shiba_running1)
+		shiba_running2 = pygame.image.load('assets/graphics/shiba/running/running_2.png').convert_alpha()
+		shiba_running2 = pygame.transform.scale2x(shiba_running2)
+		shiba_running3 = pygame.image.load('assets/graphics/shiba/running/running_3.png').convert_alpha()
+		shiba_running3 = pygame.transform.scale2x(shiba_running3)
+		shiba_running4 = pygame.image.load('assets/graphics/shiba/running/running_4.png').convert_alpha()
+		shiba_running4 = pygame.transform.scale2x(shiba_running4)
+		shiba_running5 = pygame.image.load('assets/graphics/shiba/running/running_5.png').convert_alpha()
+		shiba_running5 = pygame.transform.scale2x(shiba_running5)
+		self.shiba_running = [shiba_running1, shiba_running2, shiba_running3, shiba_running4, shiba_running5]
+		self.shiba_running_index = 0
 
-        self.image = self.shiba_running[self.shiba_running_index]
-        self.rect = self.image.get_rect(bottomleft = (80, 300))
-        self.gravity = 0
-    
-    def player_input(self):
-        global jump_debounce, flip_debounce, start_time
-        current_time = pygame.time.get_ticks() - start_time
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE] and self.rect.bottom >= 300 and jump_debounce:
-            self.gravity = -17
-            jump_debounce = False
-            start_time = pygame.time.get_ticks()
-        elif self.rect.bottom >= 300 and not jump_debounce and current_time >= 250:
-            jump_debounce = True
-        elif keys[pygame.K_SPACE] and not jump_debounce and flip_debounce and current_time >= 250:
-            self.gravity = -15
-            self.shiba_jumping_index = 0
-            flip_debounce = False
-        elif jump_debounce:
-            flip_debounce = True
-    
-    def apply_gravity(self):
-        self.gravity += 1
-        self.rect.y += self.gravity
-        if self.rect.bottom >= 300:
-            self.rect.bottom = 300
-    
-    def animation_player(self):
-        if not jump_debounce:
-            self.shiba_jumping_index += 0.15
-            if self.shiba_jumping_index >= len(self.shiba_jumping): self.shiba_jumping_index = 0
-            self.image = self.shiba_jumping[int(self.shiba_jumping_index)]
-        else:
-            self.shiba_jumping_index = 0
-            self.shiba_running_index += 0.3
-            if self.shiba_running_index >= len(self.shiba_running): self.shiba_running_index = 0
-            self.image = self.shiba_running[int(self.shiba_running_index)]
-        
-    def update(self):
-        self.player_input()
-        self.apply_gravity()
-        self.animation_player()
+		# Importing frames for jumping animation
+		shiba_jumping1 = pygame.image.load('assets/graphics/shiba/jumping/jumping_1.png').convert_alpha()
+		shiba_jumping1 = pygame.transform.scale2x(shiba_jumping1)
+		shiba_jumping2 = pygame.image.load('assets/graphics/shiba/jumping/jumping_2.png').convert_alpha()
+		shiba_jumping2 = pygame.transform.scale2x(shiba_jumping2)
+		shiba_jumping3 = pygame.image.load('assets/graphics/shiba/jumping/jumping_3.png').convert_alpha()
+		shiba_jumping3 = pygame.transform.scale2x(shiba_jumping3)
+		shiba_jumping4 = pygame.image.load('assets/graphics/shiba/jumping/jumping_4.png').convert_alpha()
+		shiba_jumping4 = pygame.transform.scale2x(shiba_jumping4)
+		shiba_jumping5 = pygame.image.load('assets/graphics/shiba/jumping/jumping_5.png').convert_alpha()
+		shiba_jumping5 = pygame.transform.scale2x(shiba_jumping5)
+		self.shiba_jumping = [shiba_jumping1, shiba_jumping2, shiba_jumping3, shiba_jumping4, shiba_jumping5]
+		self.shiba_jumping_index = 0
+
+		self.image = self.shiba_running[self.shiba_running_index]
+		self.rect = self.image.get_rect(bottomleft = (80, 300))
+		self.gravity = 0
+
+		self.mask = pygame.mask.from_surface(self.image)
+	
+	def player_input(self):
+		global jump_debounce, flip_debounce, start_time
+		current_time = pygame.time.get_ticks() - start_time
+		keys = pygame.key.get_pressed()
+		if keys[pygame.K_SPACE] and self.rect.bottom >= 300 and jump_debounce:
+			self.gravity = -17
+			jump_debounce = False
+			start_time = pygame.time.get_ticks()
+		elif self.rect.bottom >= 300 and not jump_debounce and current_time >= 250:
+			jump_debounce = True
+		elif keys[pygame.K_SPACE] and not jump_debounce and flip_debounce and current_time >= 250:
+			self.gravity = -15
+			self.shiba_jumping_index = 0
+			flip_debounce = False
+		elif jump_debounce:
+			flip_debounce = True
+
+		# Mask update
+		self.mask = pygame.mask.from_surface(self.image)
+	
+	def apply_gravity(self):
+		self.gravity += 1
+		self.rect.y += self.gravity
+		if self.rect.bottom >= 300:
+			self.rect.bottom = 300
+	
+	def animation_player(self):
+		if not jump_debounce:
+			self.shiba_jumping_index += 0.15
+			if self.shiba_jumping_index >= len(self.shiba_jumping): self.shiba_jumping_index = 0
+			self.image = self.shiba_jumping[int(self.shiba_jumping_index)]
+		else:
+			self.shiba_jumping_index = 0
+			self.shiba_running_index += 0.3
+			if self.shiba_running_index >= len(self.shiba_running): self.shiba_running_index = 0
+			self.image = self.shiba_running[int(self.shiba_running_index)]
+		
+	def update(self):
+		self.player_input()
+		self.apply_gravity()
+		self.animation_player()
 
 class Hands(pygame.sprite.Sprite):
-    def __init__(self, type) -> None:
-        super().__init__()
-        if type == "hand_1":
-            hand = pygame.image.load('assets/graphics/hands/hand_1.png').convert_alpha()
-            hand = pygame.transform.scale2x(hand)
-            y_pos = randint(140, 240)
-            self.image = hand
-            self.rect = self.image.get_rect(midtop = (randint(950,1050), y_pos))
-        elif type == "hand_2":
-            hand = pygame.image.load('assets/graphics/hands/hand_2.png').convert_alpha()
-            hand = pygame.transform.scale2x(hand)
-            y_pos = randint(180,250)
-            self.image = hand
-            self.rect = self.image.get_rect(midbottom = (randint(900,950), y_pos))
-    
-    def update(self):
-        self.rect.x -= 6
-        self.destroy()
-        self.score_checker()
-    
-    def destroy(self):
-        if self.rect.x <= -100:
-            self.kill()
-    
-    def score_checker(self):
-        if 75 <= self.rect.x <= 80:
-            score_counter(1)
+	def __init__(self, type) -> None:
+		super().__init__()
+		if type == "hand_1":
+			hand = pygame.image.load('assets/graphics/hands/hand_1.png').convert_alpha()
+			hand = pygame.transform.scale2x(hand)
+			y_pos = randint(140, 240)
+			self.image = hand
+			self.rect = self.image.get_rect(midtop = (randint(950,1050), y_pos))
+		elif type == "hand_2":
+			hand = pygame.image.load('assets/graphics/hands/hand_2.png').convert_alpha()
+			hand = pygame.transform.scale2x(hand)
+			y_pos = randint(180,250)
+			self.image = hand
+			self.rect = self.image.get_rect(midbottom = (randint(900,950), y_pos))
+		
+		self.mask = pygame.mask.from_surface(self.image)
+	
+	def update(self):
+		self.rect.x -= 6
+		self.destroy()
+		self.score_checker()
+	
+	def destroy(self):
+		if self.rect.x <= -100:
+			self.kill()
+	
+	def score_checker(self):
+		if 75 <= self.rect.x <= 80:
+			score_counter(1)
 
 class Ground(pygame.sprite.Sprite):
-    def __init__(self, start_position) -> None:
-        super().__init__()
-        sky = pygame.image.load('assets/graphics/environment/ground.png')
-        self.image = sky
-        self.rect = self.image.get_rect(topleft = (start_position, 300))
-    
-    def update(self):
-        self.rect.x -= 6
-        self.destroy()
-    
-    def destroy(self):
-        if self.rect.x <= -380:
-            self.kill()
+	def __init__(self, start_position) -> None:
+		super().__init__()
+		sky = pygame.image.load('assets/graphics/environment/ground.png')
+		self.image = sky
+		self.rect = self.image.get_rect(topleft = (start_position, 300))
+	
+	def update(self):
+		self.rect.x -= 6
+		self.destroy()
+	
+	def destroy(self):
+		if self.rect.x <= -380:
+			self.kill()
+
 
 def collision_sprite():
     global current_score
-    if pygame.sprite.spritecollide(shiba.sprite, hands, False):
-        hands.empty()
-        current_score = 0
-        shiba.sprite.rect.bottomleft = (80, 300)
-        shiba.sprite.gravity = 0
-        return False
+    for hand in hands:
+        hand_mask_offset = (hand.rect.x - shiba.sprite.rect.x, hand.rect.y - shiba.sprite.rect.y)
+
+        if shiba.sprite.mask.overlap(hand.mask, hand_mask_offset):
+            # Collision occurred
+            hands.empty()
+            current_score = 0
+            shiba.sprite.rect.bottomleft = (80, 300)
+            shiba.sprite.gravity = 0
+            return False
+
     return True
 
 def score_counter(score=0):
-    global current_score
-    current_score += score
-    score_surf = game_font.render(f'{current_score}', False, (180, 180, 180))
-    score_rect = score_surf.get_rect(midtop = (400, 25))
-    screen.blit(score_surf, score_rect)
+	global current_score
+	current_score += score
+	score_surf = game_font.render(f'{current_score}', False, (180, 180, 180))
+	score_rect = score_surf.get_rect(midtop = (400, 25))
+	screen.blit(score_surf, score_rect)
 
 # Initialize
 pygame.init()
@@ -175,40 +190,40 @@ hand_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(hand_timer, 1100)
 
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-        if not game_active:
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                    game_active = True
-        else:
-            if event.type == hand_timer:
-                hands.add(Hands(choice(['hand_1', 'hand_2', 'hand_1', 'hand_1'])))
-    
-    if game_active:
-        screen.blit(sky_surf, (0,0))
-        screen.blit(ground_surf, (0, 300))
-        score_counter()
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			exit()
+		if not game_active:
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+					game_active = True
+		else:
+			if event.type == hand_timer:
+				hands.add(Hands(choice(['hand_1', 'hand_2', 'hand_1', 'hand_1'])))
+	
+	if game_active:
+		screen.blit(sky_surf, (0,0))
+		screen.blit(ground_surf, (0, 300))
+		score_counter()
 
-        # Shiba
-        shiba.draw(screen)
-        shiba.update()
+		# Shiba
+		shiba.draw(screen)
+		shiba.update()
 
-        # Hands
-        hands.draw(screen)
-        hands.update()
+		# Hands
+		hands.draw(screen)
+		hands.update()
 
-        # Ground
-        #ground.draw(screen)
-        #ground.update()
+		# Ground
+		#ground.draw(screen)
+		#ground.update()
 
-        # Collisions
-        game_active = collision_sprite()
+		# Collisions
+		game_active = collision_sprite()
 
-    else:
-        screen.blit(game_over_surf, game_over_rect)
-        screen.blit(restart_game_surf, restart_game_rect)
-    
-    pygame.display.update()
-    clock.tick(60)
+	else:
+		screen.blit(game_over_surf, game_over_rect)
+		screen.blit(restart_game_surf, restart_game_rect)
+	
+	pygame.display.update()
+	clock.tick(60)
